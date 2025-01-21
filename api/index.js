b19b12ab07c4f0c0ccb2e276a54650b7214709b4
ankit <ankitkarapurkar444@gmail.com>
2025-01-20 23:50:18 -0800

    My commit

--- api/index.js
+++ api/index.js
@@ -1,65 +1,28 @@
+import './config/instrument.js';
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-
-dotenv.config();
-
-mongoose
-  .connect('mongodb+srv://ankitkarapurkar444:uicrrmzSFW925NJc@cluster0.ywd9w.mongodb.net/TestforERP?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-const __dirname = path.resolve();
-
+import bodyParser from 'body-parser'; 
+import cors from 'cors'; 
+import connectDB from './config/mongodb.js';
+import { config } from 'dotenv'; 
+import * as Sentry from "@sentry/node";
+config();
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(bodyParser.json()); 
+app.use(cors()); 
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js app!');
 });
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-/*
-
-app.use(cors({
-  origin: "*",
-  optionsSuccessStatus: 200, // Allow all origins (for development purposes; adjust for production)
-}));
-
-
-
-*/
-
-
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
   });
+connectDB();
+
+const PORT = process.env.PORT || 3000; 
+//asign sentry after port//
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
