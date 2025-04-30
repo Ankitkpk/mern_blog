285fa904266b24610a67e81649554a548a5b4e92
ankit <ankitkarapurkar444@gmail.com>
2025-04-30 11:03:30 -0700

    second commit

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React , { useEffect } from 'react';
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setOtherUsers } from '../redux/userSlice';
@@ -13,6 +13,7 @@ const useGetOtherUsers = () => {
                 axios.defaults.withCredentials = true;
                 const res = await axios.get(`${BASE_URL}/api/v1/user`);
                 // store
+                console.log(res);
                 console.log("other users -> ",res);
                 dispatch(setOtherUsers(res.data));
             } catch (error) {
