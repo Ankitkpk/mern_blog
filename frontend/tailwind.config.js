d3c73e086df7a375a6ef58c7f256a280c844e862
ankit <ankitkarapurkar444@gmail.com>
2025-04-30 08:07:50 -0700

    first commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
